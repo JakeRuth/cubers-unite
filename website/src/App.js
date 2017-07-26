8a5db1d26ae15f0ef5e92f00f80bfd11555e8d96
Jake Ruth <jruth@albany.edu>
2017-07-26 13:24:20 -0400

    Ran npm module react-create-app to generate scaffolding for the front end website, stripped out all the content I dont want.

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        Cubers Unite!
+      </div>
+    );
+  }
+}
