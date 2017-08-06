c18611b6c1fdf9e58c14ca81bf3fa19b47fd86dc
Jake Ruth <jruth@albany.edu>
2017-08-06 18:28:45 -0400

    Wrote test for AppHeader

---
+++ website/src/AppHeader.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppHeader from './AppHeader';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AppHeader />, div);
+});
