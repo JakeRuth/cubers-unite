71bde6f8a09fd978a85109194ff53c1468654c12
Jake Ruth <jruth@albany.edu>
2017-08-06 18:23:50 -0400

    Added Pure Css to project.  Created AppHeader.

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import AppHeader from'./AppHeader';
+
 import './App.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="app-container">
-        Cubers Unite!
+      <div className="pure-g">
+        <AppHeader/>
       </div>
     );
   }
---
+++ website/src/AppHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './AppHeader.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="pure-u-1 header-container">
+        <h1 className="title">Cubers Unite</h1>
+      </div>
+    );
+  }
+}
