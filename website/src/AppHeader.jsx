0f07c71332873739d96f5b608a7776d91441f62e
Jake Ruth <jruth@albany.edu>
2017-08-08 22:53:41 -0400

    Created simple actionless sign up form.  Downloaded purecss file, now linked locally to fix bug.

--- website/src/AppHeader.jsx
+++ website/src/AppHeader.jsx
@@ -5,7 +5,7 @@ import './AppHeader.css';
 export default class App extends React.Component {
   render() {
     return (
-      <div className="pure-u-1 header-container">
+      <div className="app-header-container pure-u-1">
         <h1 className="title">Cubers Unite</h1>
       </div>
     );
