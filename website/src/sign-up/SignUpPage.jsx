1aeb746050abe03a05fbe4d95aa8a6d94b5bb5b7
Jake Ruth <jruth@albany.edu>
2017-08-27 17:45:19 -0400

    Changed colors of UI elements.  Added two labels to sign up form, one is a link that isn't functional yet

--- website/src/sign-up/SignUpPage.jsx
+++ website/src/sign-up/SignUpPage.jsx
@@ -23,42 +23,46 @@ class SignUpPageComponent extends React.Component {
     );
   }
 
-  renderForm() {
+  renderContent() {
     return (
-      <form className="sign-up-page-container pure-form pure-form-aligned">
-          <div className="pure-control-group">
-              <input
-                id="email"
-                placeholder="Email Address"
-                value={this.props.email}
-                onChange={this.props.updateEmail}
-              />
-          </div>
-          <div className="pure-control-group">
-              <input
-                id="name"
-                placeholder="Username"
-                value={this.props.username}
-                onChange={this.props.updateUsername}
-              />
-          </div>
-          <div className="pure-control-group">
-              <input
-                id="password"
-                type="password"
-                placeholder="Password"
-                value={this.props.password}
-                onChange={this.props.updatePassword}
-              />
-          </div>
-          <button
-            onClick={this.onSignUpSubmit}
-            className="pure-button pure-button-primary"
-            type="button"
-          >
-            Submit
-          </button>
-      </form>
+      <div>
+        <p className="sign-up-form-label">Sign up to start cubing</p>
+        <form className="sign-up-page-container pure-form pure-form-aligned">
+            <div className="pure-control-group">
+                <input
+                  id="email"
+                  placeholder="Email Address"
+                  value={this.props.email}
+                  onChange={this.props.updateEmail}
+                />
+            </div>
+            <div className="pure-control-group">
+                <input
+                  id="name"
+                  placeholder="Username"
+                  value={this.props.username}
+                  onChange={this.props.updateUsername}
+                />
+            </div>
+            <div className="pure-control-group">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  value={this.props.password}
+                  onChange={this.props.updatePassword}
+                />
+            </div>
+            <button
+              onClick={this.onSignUpSubmit}
+              className="pure-button pure-button-primary"
+              type="button"
+            >
+              Submit
+            </button>
+        </form>
+        <p className="token-form-link">Already have a verification code? Click here.</p>
+      </div>
     );
   }
 
@@ -70,7 +74,7 @@ class SignUpPageComponent extends React.Component {
         content = this.renderSpinner();
         break;
       default:
-        content = this.renderForm();
+        content = this.renderContent();
         break;
     }
 
