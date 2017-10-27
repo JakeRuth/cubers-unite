59cb4d0fbfd551bfb3a39beb62fe65ba8e39b72c
Jake Ruth <jruth@albany.edu>
2017-10-26 21:00:31 -0400

    modified create room endpoint to use cors.  Successfully tested it with ajax call woohoo!

--- aws-serverless/room/createRoom.py
+++ aws-serverless/room/createRoom.py
@@ -3,8 +3,13 @@ import json
 def handler(event, context):
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Credentials" : True,
+        },
         "body": json.dumps({
             "message": "woop woop",
             "input": event,
+            'suck': 'my nuts',
         }),
     }
