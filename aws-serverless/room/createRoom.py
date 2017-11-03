e20243c8be1bbd7f4fda7a445ae3968845a37abe
Jake Ruth <jruth@albany.edu>
2017-11-02 22:21:27 -0400

    Added save Room funcationality using data from the request, yeeey!! #BallsRolling

--- aws-serverless/room/createRoom.py
+++ aws-serverless/room/createRoom.py
@@ -1,6 +1,24 @@
 import json
+import boto3
+import uuid
 
 def handler(event, context):
+    client = boto3.client('dynamodb')
+    body = json.loads(event['body'])
+    name = body['name']
+
+    response = client.put_item(
+        TableName='Room',
+        Item={
+            'id': {
+                'S': str(uuid.uuid4()),
+            },
+            'name': {
+                'S': name,
+            },
+        },
+    )
+
     return {
         "statusCode": 200,
         "headers": {
