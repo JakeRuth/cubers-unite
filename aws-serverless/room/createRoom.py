a9f97121fc9e5b2ba4dbd76df570e84f088fb714
Jake Ruth <jruth@albany.edu>
2018-02-13 22:18:09 -0500

    Create room user when creating room

--- aws-serverless/room/createRoom.py
+++ aws-serverless/room/createRoom.py
@@ -8,38 +8,36 @@ def handler(event, context):
     body = json.loads(event['body'])
     name = body['name']
     username = body['username']
+    user_id = body['userId']
 
-    response = client.put_item(
+    room_id = str(uuid.uuid4())
+    client.put_item(
         TableName='Room',
         Item={
-            'id': {
-                'S': str(uuid.uuid4()),
-            },
-            'name': {
-                'S': name,
-            },
-            'puzzleType': {
-                'S': '3x3',
-            },
-            'createdBy': {
-                'S': username,
-            },
-            'createdAt': {
-                'S': str(datetime.datetime.utcnow()),
-            },
-            'status': {
-                'S': 'active',
-            },
+            'id': {'S': room_id},
+            'name': {'S': name},
+            'puzzleType': {'S': '3x3'},
+            'createdBy': {'S': username},
+            'createdAt': {'S': str(datetime.datetime.utcnow())},
+            'status': {'S': 'active'},
+        },
+    )
+    client.put_item(
+        TableName='RoomUser',
+        Item={
+            'id': {'S': str(uuid.uuid4())},
+            'roomId': {'S': room_id},
+            'userId': {'S': user_id},
+            'createdAt': {'S': str(datetime.datetime.utcnow())},
+            'status': {'S': 'active'},
         },
     )
 
     return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Credentials" : True,
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Credentials' : True,
         },
-        "body": json.dumps({
-            "input": event,
-        }),
+        'body': json.dumps({}),  # empty response
     }
